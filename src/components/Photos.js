66b84b9794a0c599b33792db8608fa7fcbd02610
Danny Bevan <danbev82@gmail.com>
2022-05-27 16:23:30 +0100

    initial commit

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,16 +1,34 @@
-import photo1 from "./images/test/DSC_0214.JPG";
-import photo2 from "./images/test/DSC_0215.JPG";
-import photo3 from "./images/test/DSC_0305.JPG";
-import photo4 from "./images/test/DSC_0217.JPG";
-import photo5 from "./images/test/DSC_0218.JPG";
-import photo6 from "./images/test/DSC_0304.JPG";
-import photo7 from "./images/test/DSC_0367.JPG";
-import photo8 from "./images/test/DSC_0231.JPG";
-import photo9 from "./images/test/DSC_0232.JPG";
-import photo10 from "./images/test/DSC_0389.JPG";
-import photo11 from "./images/test/DSC_0380.JPG";
-import photo12 from "./images/test/DSC_0248.JPG";
-import photo13 from "./images/test/DSC_0247.JPG";
+import photo1 from "./images/1.JPG";
+import photo2 from "./images/2.JPG";
+import photo3 from "./images/3.JPG";
+import photo4 from "./images/4.JPG";
+import photo5 from "./images/5.JPG";
+import photo6 from "./images/6.JPG";
+import photo7 from "./images/7.JPG";
+import photo8 from "./images/8.JPG";
+import photo9 from "./images/9.JPG";
+import photo10 from "./images/10.JPG";
+import photo11 from "./images/11.JPG";
+import photo12 from "./images/12.JPG";
+import photo13 from "./images/13.JPG";
+import photo14 from "./images/14.JPG";
+import photo15 from "./images/15.JPG";
+import photo16 from "./images/16.JPG";
+import photo17 from "./images/17.JPG";
+import photo18 from "./images/18.JPG";
+import photo19 from "./images/19.JPG";
+import photo20 from "./images/20.JPG";
+import photo21 from "./images/21.JPG";
+import photo22 from "./images/22.JPG";
+import photo23 from "./images/23.JPG";
+import photo24 from "./images/24.JPG";
+import photo25 from "./images/25.JPG";
+import photo26 from "./images/26.JPG";
+import photo27 from "./images/27.JPG";
+import photo28 from "./images/28.JPG";
+import photo29 from "./images/29.JPG";
+import photo30 from "./images/30.JPG";
+import photo31 from "./images/31.JPG";
 
 export const photos = [
     {
@@ -74,6 +92,95 @@ export const photos = [
     },
     {
         src: photo13,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo14,
+        desc: "A load of nonesense to fill this bit ",
+        sub: "A photo of something that was there "
+    },
+    {
+        src: photo15,
+        desc: "a picture ",
+        sub: " some words to test some shit "
+    },    {
+        src: photo16,
+        desc: "angry tiger ran after the fat beaver only to find a hairy fish in its stead ",
+        sub: " hushdushduhd"
+    },
+    {
+        src: photo17,
+        desc: "iwidwid djdjdkdk d d d d d dknihd ",
+        sub: " gggggggggggggggg"
+    },
+    {
+        src: photo18,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo19,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo20,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo21,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo22,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo23,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo24,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo25,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo26,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo27,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo28,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo29,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo30,
+        desc: " ",
+        sub: " "
+    },
+    {
+        src: photo31,
         desc: " ",
         sub: " "
     }
